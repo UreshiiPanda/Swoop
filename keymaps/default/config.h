245cc94a715f47565177dd59123b9b68033a18b0
UreshiiPanda <jojohoughton22@gmail.com>
2024-09-22 19:22:28 -0500

    migrated from vial to qmk, added leader key

---
+++ keymaps/default/config.h
@@ -0,0 +1,60 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//#define VIAL_KEYBOARD_UID {0xFD, 0x91, 0xD2, 0xB9, 0xFA, 0xD5, 0x59, 0xAB}
+
+
+#define RGBLIGHT_ANIMATIONS
+//#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+//#define VIAL_TAP_DANCE_ENTRIES 8
+#define NO_ACTION_ONESHOT
+
+
+// OLED driver
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X32
+  #define OLED_TIMEOUT 30000
+  #define SPLIT_OLED_ENABLE
+#endif
+
+// RGB underglow and per key
+//#define RGB_DI_PIN D3
+#ifdef RGB_DI_PIN
+  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+  #define RGBLIGHT_EFFECT_BREATHING
+  #define RGBLED_NUM 36
+  #define RGBLED_SPLIT \
+        { 18, 18 }
+  #define RGBLIGHT_SPLIT
+  #define RGBLIGHT_LIMIT_VAL 150
+  #define RGBLIGHT_HUE_STEP 10
+  #define RGBLIGHT_SAT_STEP 17
+  #define RGBLIGHT_VAL_STEP 17
+  #define RGBLIGHT_SLEEP
+#endif
+
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#undef RGBLIGHT_ANIMATIONS
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
+
+
+// the maximum time (ms) allowed between taps of your Tap Dance key
+#define TAPPING_TERM 150
+
+// you can enable per-key timing to ensure that each key pressed provides you with more time to finish the sequence
+#define LEADER_PER_KEY_TIMING
+// This is the amount of time you have to complete a sequence once the leader key has been pressed
+#define LEADER_TIMEOUT 200
+
---
+++ keymaps/default/rules.mk
@@ -0,0 +1,47 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+LEADER_ENABLE = yes         # Leader Key
+
+SPLIT_KEYBOARD = yes
+OLED_DRIVER_ENABLE = yes
+
+LAYOUTS = split_3x5_3
+
+
+#VIA_ENABLE = yes
+#VIAL_ENABLE = yes
+#VIAL_INSECURE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+LEADER_ENABLE = yes
+
+
+STENO_ENABLE = no
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+VELOCIKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+QMK_SETTINGS = no
+ENCODER_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+
